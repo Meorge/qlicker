5dfa00606bd7806e4a6f689c071a93596d288214
Malcolm Anderson <malcolminyo@gmail.com>
2020-01-04 02:09:31 +0000

    Improvements to course search system

--- imports/ui/pages/professor/manage_courses.jsx
+++ imports/ui/pages/professor/manage_courses.jsx
@@ -4,6 +4,9 @@
 // manage_courses.jsx: all courses (active & inactive) page
 
 import React, { Component } from 'react'
+import Select from 'react-select'
+import 'react-select/dist/react-select.css'
+
 // import ReactDOM from 'react-dom'
 import { createContainer } from 'meteor/react-meteor-data'
 
@@ -17,12 +20,21 @@ class _ManageCourses extends Component {
   constructor (props) {
     super(props)
 
-    this.state = { creatingCourse: false }
+    this.state = { 
+      creatingCourse: false
+     }
 
     this.doneCreatingCourse = this.doneCreatingCourse.bind(this)
     this.promptCreateCourse = this.promptCreateCourse.bind(this)
     this.deleteCourse = this.deleteCourse.bind(this)
     this.setCourseActive = this.setCourseActive.bind(this)
+
+    this.collectCourseInformation = this.collectCourseInformation.bind(this)
+  }
+
+  componentDidMount () {
+    Meteor.setTimeout(this.collectCourseInformation, 500)
+    // this.collectCourseInformation()
   }
 
   deleteCourse (courseId) {
@@ -30,6 +42,7 @@ class _ManageCourses extends Component {
       Meteor.call('courses.delete', courseId, (error) => {
         if (error) return alertify.error('Error deleting course')
       })
+      Meteor.setTimeout(this.collectCourseInformation, 500)
     }
   }
 
@@ -49,22 +62,60 @@ class _ManageCourses extends Component {
   doneCreatingCourse (e) {
     this.setState({ creatingCourse: false })
     Meteor.setTimeout(this.forceUpdate, 500)
+    Meteor.setTimeout(this.collectCourseInformation, 500)
+  }
+
+  collectCourseInformation () {
+    // Get a list of all of the departments
+    depts = []
+    sems = []
+
+    for (index = 0; index < this.props.courses.length; index++) {
+      if (!depts.includes(this.props.courses[index].deptCode)) {
+        var b = this.props.courses[index].deptCode.toUpperCase()
+        depts.push( { value: b, label: b } )
+      }
+
+      if (!sems.includes(this.props.courses[index].semester)) {
+        var b = this.props.courses[index].semester.toUpperCase()
+        sems.push( { value: b, label: b } )
+      }
+    }
+
+    this.setState({ deptCodes: depts })
+    this.setState({ sems: sems })
   }
 
   renderCourseList (cList) {
-    
     // Search filtering
     if ( this.state.filterClassSearchString ) {
       cList = _(cList).filter(function (crs) {
-        return crs.name.toLowerCase().includes(this.state.filterClassSearchString.toLowerCase())
-        || crs.deptCode.toLowerCase().includes(this.state.filterClassSearchString.toLowerCase())
-        || crs.courseNumber.toLowerCase().includes(this.state.filterClassSearchString.toLowerCase())
-        || crs.section.toLowerCase().includes(this.state.filterClassSearchString.toLowerCase())
-        || crs.semester.toLowerCase().includes(this.state.filterClassSearchString.toLowerCase())
+        var searched = this.state.filterClassSearchString.toLowerCase()
+        return crs.name.toLowerCase().includes(searched)
+        || crs.deptCode.toLowerCase().includes(searched)
+        || crs.courseNumber.toLowerCase().includes(searched)
+        || crs.section.toLowerCase().includes(searched)
+        || crs.semester.toLowerCase().includes(searched)
+        || (crs.name + " " + crs.deptCode + " " + crs.courseNumber + " " + crs.section + " " + crs.semester).toLowerCase().includes(searched)
       }.bind(this))
     }
+
+    if ( this.state.searchDept  && this.state.searchDept.length > 0 ){
+      cList = cList.filter( function (crs) {
+        var lcArray = _(this.state.searchDept).pluck('value').map(v => v.toLowerCase());
+        return lcArray.includes(crs.deptCode.toLowerCase())
+      }.bind(this))
+    }
+
+    if ( this.state.searchSem  && this.state.searchSem.length > 0 ){
+      cList = cList.filter( function (crs) {
+        var lcArray = _(this.state.searchSem).pluck('value').map(v => v.toLowerCase());
+        return lcArray.includes(crs.semester.toLowerCase())
+      }.bind(this))
+    }
+
     return cList.map( (course) => {
-      console.log(Object.keys(course))
+      
       controls = []
       if (course.inactive) {
         controls.push( { label: 'Make active', click: () => this.setCourseActive(course._id, true) } )
@@ -88,11 +139,23 @@ class _ManageCourses extends Component {
     this.setState( {filterClassSearchString: val} )
   }
 
+
+  setFilterDeptSearchString (val) {
+    this.setState( {filterDeptSearchString: val} )
+  }
+
   render () {
     const setSearchCourse = (e) => {
-      console.log(e.target.value)
       this.setState({ searchCourse: e.target.value })// need this to update the input box
       this.setFilterClassSearchString(e.target.value)
+    }
+
+    const setSearchDept = (val) => {
+      this.setState({ searchDept: val })// need this to update the input box
+    }
+
+    const setSearchSem = (val) => {
+      this.setState({ searchSem: val })// need this to update the input box
     }
 
     return (
@@ -101,7 +164,37 @@ class _ManageCourses extends Component {
         <button className='btn btn-primary' onClick={this.promptCreateCourse}>Create Course</button>
 
         <br />
-        <input type='text' placeholder='Search...' onChange = {setSearchCourse} value={this.state.searchCourse} />
+        <div className = "container">
+          <div className = "row">
+            <div className = 'ql-search-table-container'>
+              <div className = 'ql-search'>
+                <input type='text' placeholder='Search...' onChange = {setSearchCourse} value={this.state.searchCourse} />
+                  <div className='select-container'>
+                    <Select
+                      name='search-dept'
+                      placeholder='Department'
+                      multi
+                      value={this.state.searchDept}
+                      options={this.state.deptCodes}
+                      onChange={setSearchDept}
+                      />
+                  </div>
+                  <div className='select-container'>
+                    <Select
+                      name='search-sem'
+                      placeholder='Semester'
+                      multi
+                      value={this.state.searchSem}
+                      options={this.state.sems}
+                      onChange={setSearchSem}
+                      />
+                  </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
         <div className='ql-courselist'>
           { this.renderCourseList(this.props.courses.filter((c) => !c.inactive)) }
         </div>
--- server/secret.js
+++ server/secret.js
@@ -1,6 +1,6 @@
 smtp = {
-   username: "emailAddress@email.com",
-   password: "password",
-   server: "smtp.email.com",
+   username: "clarkclickerras+origin@gmail.com",
+   password: "gyrjux-heznoJ-0kaqwy",
+   server: "smtp.gmail.com",
    port: 465
 }
